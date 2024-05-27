e9790652b5dd33cb2e3e2dedae6d36b08dcf6d57
Desond Anane <doanane@st.ug.edu.gh>
2024-05-27 00:32:13 +0000

    final output

--- assignment2/App.js
+++ assignment2/App.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{flex: 1, backgroundColor: '#f4ce14'}}>
-      <Text style={{padding:80, fontSize: 26,color: "black"}}>
+      <Text style={{padding:80, fontSize: 24,color: "black"}}>
       My name is { ' '}
       <Text style={{fontWeight: 'bold'}}>
-      Desmond Opoku Anane.</Text>
+      Desmond Opoku</Text>
       </Text>
       <StatusBar style="auto" />
     </View>
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    fontSize: 26,
+    fontSize: 24,
     backgroundColor: '#f4ce14',
     alignItems: 'center',
     justifyContent: 'center',
